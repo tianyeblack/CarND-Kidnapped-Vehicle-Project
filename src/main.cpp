08822eb542bb71fe7abd52455942c0d3570afb41
Ye Tian <tt.willturner@gmail.com>
2018-11-10 19:32:40 -0800

    Listen on Port 3000 instead of 4567

--- src/main.cpp
+++ src/main.cpp
@@ -177,7 +177,7 @@ int main()
     std::cout << "Disconnected" << std::endl;
   });
 
-  int port = 4567;
+  int port = 3000;
   if (h.listen(port))
   {
     std::cout << "Listening to port " << port << std::endl;
